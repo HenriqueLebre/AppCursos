77a27923bd212d3c90f375e16de54e0bc2603bab
henriqueLebre <henrique_lebre@hotmail.com>
2023-09-11 23:38:57 -0300

    Criando modulos de subs, users e notificacoes.

---
+++ SubscriptionContext/Student.cs
@@ -0,0 +1,30 @@
+using System.Security.Cryptography.X509Certificates;
+using Balta.NotificationContext;
+using Balta.SharedContext;
+
+namespace Balta.SubscriptionContext;
+
+public class Student : Base
+{
+    public Student()
+    {
+        Subscriptions = new List<Subscription>();
+    }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public User User { get; set; }
+    public IList<Subscription> Subscriptions { get; set; }
+
+    public void CreateSubscription(Subscription subscription)
+    {
+        if (IsPremium)
+        {
+            AddNotification(new Notification("Premium", "O Aluno já possui uma assinatura ativa."));
+            return;
+        }
+        Subscriptions.Add(subscription);
+    }
+
+    public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
+
+}
